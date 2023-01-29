dcee2f7b3112bda6f56518fa3bc098309c0e6b3a
Martin Dörfelt <martin.d@andix.de>
2023-01-29 13:56:26 +0100

    skip anything venv related to make debugging easier

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 # setup venv
-if [ ! -d .venv ]; then
-    python3 -m venv --system-site-packages .venv
-    . .venv/bin/activate
-    python3 -m pip install -r requirements.txt
-else
-    . .venv/bin/activate
-fi
+# if [ ! -d .venv ]; then
+#     python3 -m venv --system-site-packages .venv
+#     . .venv/bin/activate
+#     python3 -m pip install -r requirements.txt
+# else
+#     . .venv/bin/activate
+# fi
+python3 -m pip install -r requirements.txt
 
 # clear and build
 rm -rf dist
 python3 -OO -m PyInstaller "pyinstaller_files/build_linux.spec" --noconfirm
 
 # exit venv
-deactivate
+# deactivate
