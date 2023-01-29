b2202146ded4494d4c6c72300b27e9a6b5c5860b
Martin Dörfelt <martin.d@andix.de>
2023-01-29 13:06:29 +0100

    add system dependencies

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# https://pygobject.readthedocs.io/en/latest/getting_started.html
+if [ "$1" = "linux" ]; then
+    apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0
+elif [ "$1" = "macos" ]; then
+    brew install pygobject3 gtk+3
+elif [ "$1" = "windows" ]; then
+    pacman -S mingw-w64-x86_64-gtk3 mingw-w64-x86_64-python3 mingw-w64-x86_64-python3-gobject
+else
+    exit 1
+fi
